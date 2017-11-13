9233dfe844d732bd553c3112aa4408e1be32147c
thienth32 <thienth32@gmail.com>
2017-11-13 15:23:19 +0700

    update

--- l31/public/js/lib.js
+++ l31/public/js/lib.js
@@ -1,6 +1,31 @@
 window.lib = {
-	test: function(){
-		console.log('hello world!!!');
+	ip: document.getElementById('strIp'),
+	result: document.getElementById('result'),
+	getLength: function(){
+		var val = this.ip.value;
+		result.innerText = `Do dai cua chuoi = ${val.length} ky tu`;
+	},
+	findIndex: function(){
+		var val = this.ip.value;
+
+		var index = val.indexOf('t3h');
+		if(index != -1){
+			result.innerText 
+							= `ky tu "t3h" co xuat hien trong chuoi (${index})`;
+		}else{
+			result.innerText 
+							= `ky tu "t3h" khong xuat hien trong chuoi`;
+		}
+	},
+	validLength: function(){
+		var val = this.ip.value;
+		if(val.length >= 5 && val.length <= 15){
+			result.innerText 
+							= `Hợp lệ!`;
+		}else{
+			result.innerText 
+							= `Không hợp lệ!`;
+		}
 	}
 }
 
